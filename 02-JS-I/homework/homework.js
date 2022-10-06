21889ed933b1a2a53cdffa35e20abff4fb3d74ef
Noelia <noelia.martino@live.com.ar>
2022-10-06 19:13:50 -0300

    Homework I

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -60,11 +60,9 @@ function sonIguales(x, y) {
   // Devuelve "true" si "x" e "y" son iguales
   // De lo contrario, devuelve "false"
   // Tu código:
-    if (x == y){
+    if (x === y){
       return true
-    } else{
-      return false
-    }
+    } else return false
 }
 
 function tienenMismaLongitud(str1, str2) {
@@ -73,9 +71,7 @@ function tienenMismaLongitud(str1, str2) {
   // Tu código:
     if (str1.length === str2.length) {
       return true
-    } else {
-      return false
-    }
+    } else return false
 }
 
 function menosQueNoventa(num) {
@@ -84,9 +80,7 @@ function menosQueNoventa(num) {
   // Tu código:
     if (num < 90) {
       return true
-    } else {
-      return false
-    }
+    } else return false
 }
 
 function mayorQueCincuenta(num) {
@@ -95,9 +89,7 @@ function mayorQueCincuenta(num) {
   // Tu código:
     if (num > 50) {
       return true
-    } else {
-      return false
-    }
+    } else return false
 }
 
 function obtenerResto(x, y) {
@@ -110,22 +102,18 @@ function esPar(num) {
   // Devuelve "true" si "num" es par
   // De lo contrario, devuelve "false"
   // Tu código:
-    if (num % 2 == 0) {
+    if (num % 2 === 0) {
       return true
-    } else {
-      return false
-    }
+    } else return false
 }
 
 function esImpar(num) {
   // Devuelve "true" si "num" es impar
   // De lo contrario, devuelve "false"
   // Tu código:
-    if (num % 2 != 0) {
+    if (num % 2 !== 0) {
       return true
-    } else {
-      return false
-    }
+    } else return false
 }
 
 function elevarAlCuadrado(num) {
@@ -174,30 +162,28 @@ function esPositivo(numero) {
       return "Es positivo"
     } else if (numero < 0){
       return "Es negativo"
-    } else {
-      return false
-    }
+    } else return false
 }
 
 function agregarSimboloExclamacion(str) {
   // Agrega un símbolo de exclamación al final de la string "str" y devuelve una nueva string
   // Ejemplo: "hello world" pasaría a ser "hello world!"
   // Tu código:
-    return str + '!'
+    return str + "!"
 }
 
 function combinarNombres(nombre, apellido) {
   // Devuelve "nombre" y "apellido" combinados en una string y separados por un espacio.
   // Ejemplo: "Soy", "Henry" -> "Soy Henry"
   // Tu código:
-    return nombre + ' ' + apellido
+    return nombre + " " + apellido
 }
 
 function obtenerSaludo(nombre) {
   // Toma la string "nombre" y concatena otras string en la cadena para que tome la siguiente forma:
   // "Martin" -> "Hola Martin!"
   // Tu código:
-    return 'Hola ' + nombre + '!'
+    return "Hola " + nombre + "!"
 }
 
 function obtenerAreaRectangulo(alto, ancho) {
@@ -235,14 +221,12 @@ function esVocal(letra){
   //que no se puede procesar el dato mediante el mensaje "Dato incorrecto".
   // Si no es vocal, tambien debe devolver "Dato incorrecto".
   //Escribe tu código aquí
-     if(letra === 'a' || letra === 'e' || letra === 'i' || letra === 'o' || letra === 'u'){
+     if(letra === 'a' || letra === 'e' || letra === 'i' || letra === 'o' || letra === 'u' || letra === 'A' || letra === 'E' || letra === 'I' || letra === 'O' || letra === 'U'){
       return 'Es vocal'
-     } else {
-      return 'Dato incorrecto'
-     }
+     } else if (letra.length > 1){
+      return "Dato incorrecto"
+     } else return "Dato incorrecto"
 }
-
-
 
 // No modificar nada debajo de esta línea
 // --------------------------------
